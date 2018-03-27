c78d0ec14b69cc24a080aa5786ad8945379451ba
caleb logan <clogan202@gmail.com>
2018-03-27 11:52:50 -0700

    finish channel basic ui videocards and grid

--- src/scenes/Channel/Channel.js
+++ src/scenes/Channel/Channel.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import { Menu, Container, Divider } from 'semantic-ui-react';
 
 import ChannelBanner from "./ChannelBanner";
-import styles from './Channel.css';
 import ChannelAbout from "./ChannelAbout";
 import ChannelVideos from "./ChannelVideos";
+import styles from './Channel.css';
 
 class Channel extends Component {
 
@@ -15,18 +15,85 @@ class Channel extends Component {
     this.state = {
       activeTab: 'VIDEOS',
       videos: [
-        {
-          url: '/channel/elimence/fwt6reux',
-          title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
-          username: 'elimence',
-          thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
-          playtime: '09:07',
-          rewards: '$16.516',
-          date: '5 hours ago',
-        }
+        // {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
+        //         {
+        //   url: '/channel/elimence/fwt6reux',
+        //   title: 'Music talk: Samplers, limitations, creativity, meditation, spirituality.',
+        //   username: 'elimence',
+        //   thumbnail: 'https://snap1.d.tube/ipfs/QmT2CbjZ7tv312ZZ42owPxx9nx9xfEWShGsZqDemYEjec6',
+        //   playtime: '09:07',
+        //   rewards: '$16.516',
+        //   date: '5 hours ago',
+        // },
       ],
     };
     this.onMenuClick = this.onMenuClick.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('mounting my dude')
   }
 
   onMenuClick(event, {name}) {
@@ -56,7 +123,7 @@ class Channel extends Component {
           avatarUrl={avatarUrl}
           onSubscribe={() => alert('not implemented')}
         />
-        <Container>
+        <Container className={styles.ContentContainer}>
           <Menu pointing secondary tabular attached='top' className={styles.ChannelMenu}>
             <Menu.Item name='VIDEOS' active={activeTab === 'VIDEOS'} onClick={this.onMenuClick} />
             <Menu.Item name='ABOUT' active={activeTab === 'ABOUT'} onClick={this.onMenuClick} />
--- src/scenes/Channel/ChannelVideos.js
+++ src/scenes/Channel/ChannelVideos.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import GridVideoCard from "../../components/VideoCards/GridVideoCard";
 
+import styles from './ChannelVideos.css';
+
 const ChannelVideos = ({
   videos,
 }) => {
   return (
-    <div>
+    <div className={styles.ChannelVideos}>
       {videos.map(video => (
         <GridVideoCard key={video.url} {...video} />
       ))}
